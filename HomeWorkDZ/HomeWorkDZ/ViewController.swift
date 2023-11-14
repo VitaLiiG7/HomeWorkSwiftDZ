7d0ce6bb7ab12e00e682cda0d93609b87bfe8150
Vitqqqqqqqq <130509045+Vitqqqqqqqq@users.noreply.github.com>
2023-11-14 15:18:51 +0300

    initial commit

---
+++ HomeWorkDZ/HomeWorkDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkDZ
+//
+//  Created by Виталий Горбунов on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
