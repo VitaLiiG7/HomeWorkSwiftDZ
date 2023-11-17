42682cf9b31d054fb2a128bdcba19fd3854c8c52
Vitqqqqqqqq <130509045+Vitqqqqqqqq@users.noreply.github.com>
2023-11-17 17:20:54 +0300

    homework 9

--- HomeWorkDZ/HomeWorkDZ/ViewController.swift
+++ HomeWorkDZ/HomeWorkDZ/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  HomeWorkDZ
-//
-//  Created by Виталий Горбунов on 14.11.2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    let screenSize:CGRect = UIScreen.main.bounds
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    var count = 0
+    
+    @IBAction func collapsingUnfoldingTextButton(_ sender: Any) {
+    
+        count += 1
+        
+        if count % 2 != 0 {
+            nameLabel.numberOfLines = 1
+            heightConstraint.constant = 0
+            view.layoutIfNeeded()
+            
+        } else {
+            nameLabel.numberOfLines += 4
+            heightConstraint.constant = 150
+            view.layoutIfNeeded()
+        }
+    }
 }
 
